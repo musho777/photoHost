1e0f4a721ff4278791e4545b66929d63f4baf22a
musho777 <129147695+musho777@users.noreply.github.com>
2023-07-28 10:54:49 +0400

    Новая публикация

--- src/screens/AddImg/AddImg.js
+++ src/screens/AddImg/AddImg.js
@@ -1,39 +1,86 @@
-import React, {useEffect} from 'react';
-import {
-  Image,
-  StyleSheet,
-  View,
-} from 'react-native';
+import React, {useEffect,useState} from 'react';
+import {Image, StyleSheet, TextInput, View} from 'react-native';
 import ImagePicker, {ImageOrVideo} from 'react-native-image-crop-picker';
+import { HeaderWhiteTitle } from '../../headers/HeaderWhiteTitle.';
+import { AppColors } from '../../styles/AppColors';
+import { Styles } from '../../styles/Styles';
 
 export const AddImg = ({navigation}) => {
-  const [uri, setUri] = React.useState();
+  const [uri, setUri] = useState([]);
+  const [description,setDescription] = useState('')
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', async () => {
-        ImagePicker.openPicker({
-          width: 300,
-          height: 400,
-          cropping: true,
-        }).then(image => {
-          navigation.navigate('Home')
-          setUri(image.path);
-        });
+      ImagePicker.openPicker({
+        width: 300,
+        height: 400,
+        cropping: true,
+        multiple: true,
+      }).then(image => {
+        console.log(image);
+        // navigation.navigate('Home')
+        setUri(image);
+      });
     });
     return unsubscribe;
   }, [navigation]);
 
+  const creatPost = () =>{
+
+  }
   return (
     <View>
+      <HeaderWhiteTitle
+        // loading={changeProfil.loading||changeAvatar.loading}
+        onCheck={() => creatPost()}
+        check
+        onPress={() => navigation.goBack()}
+        title={'Редактировать профиль'}
+      />
+      <View style = {styles.wrapper}>
+        {uri.map((elm, i) => {
+          return (
+            <Image
+              key={i}
+              style={styles.img}
+              source={{
+                uri: elm.path,
+              }}
+            />
+          );
+        })}
+      </View>
+      <View style={styles.textWrapper}>
+        <TextInput
+          value={description}
+          onChangeText={e => setDescription(e)}
+          style={Styles.darkMedium14}
+          placeholder = 'Добавить описание'
+          placeholderTextColor={'#8C9CAB'}
+        />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  avatar: {
-    paddingTop: 20,
-    height: 100,
-    width: 100,
-    borderRadius: 100,
-    padding: 20,
+  img: {
+    height: 85,
+    width: '22%',
+    borderRadius: 10,
+    margin:5
+  },
+  wrapper:{
+    flexDirection:'row',
+    flexWrap:'wrap',
+    marginBottom:20,
+    marginHorizontal:8,
+  },
+  textWrapper: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderBottomColor: AppColors.Solitude_Color,
+    borderTopColor:AppColors.Solitude_Color,
+    borderBottomWidth: 1,
+    borderTopWidth:1,
   },
 });
