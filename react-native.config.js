b18f0e7cf1ab880efb40b60533129212d702608a
musho777 <129147695+musho777@users.noreply.github.com>
2024-09-15 14:12:53 +0400

    ios integration

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-    project: {
-      ios: {},
-      android: {}, // grouped into "project"
-    },
-    assets: ["./src/assets/fonts/"], // stays the same
-  };
+  project: {
+    ios: {},
+    android: {}, // grouped into "project"
+  },
+  assets: ["./src/assets/fonts/"], // stays the same
+  reactNativePath: './node_modules/react-native',
+  hermes: true,
+};
